f79ef456d6eb29b3c514e7d8504fd837a1cd0e2b
vaghdevi175 <vaghdevipappala@gmail.com>
2026-03-18 12:54:08 +0530

    Removed test_db.py

--- app/test_db.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient(
-"mongodb+srv://admin:admin123@cluster0.ovje4uh.mongodb.net/virtual_tryon?retryWrites=true&w=majority&appName=Cluster0"
-)
-
-db = client["virtual_tryon"]
-
-print("MongoDB connected successfully!")
