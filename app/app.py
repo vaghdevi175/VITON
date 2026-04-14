aab870b99376487a590cc15e18cc2b0ef3b7fa35
vaghdevi175 <vaghdevipappala@gmail.com>
2026-04-14 19:59:18 +0530

    updated redirects in app.py

--- app/app.py
+++ app/app.py
@@ -128,7 +128,8 @@ def google_login():
         "client_id": GOOGLE_CLIENT_ID,
         "client_secret": GOOGLE_CLIENT_SECRET,
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token"
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "redirect_uris": [REDIRECT_URI] 
     }
 })
     flow.redirect_uri = REDIRECT_URI
@@ -159,7 +160,8 @@ def google_callback():
         "client_id": GOOGLE_CLIENT_ID,
         "client_secret": GOOGLE_CLIENT_SECRET,
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token"
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "redirect_uris": [REDIRECT_URI] 
     }
 })
     flow.redirect_uri = REDIRECT_URI
